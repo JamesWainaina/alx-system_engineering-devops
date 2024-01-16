6d0fc620717bb5b4786a36c5e703f0293576a459
JamesWainaina <jamesgatheru9905@gmail.com>
2024-01-16 17:43:54 +0300

    change config file with puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,8 +1,6 @@
-# make changes to configuration file using Puppet
-
+# change SSH config file
 exec { 'echo':
-    path    => 'usr/bin:/bin',
-    command => 'echo " IdentifyFile ~/.ssh/school\n
-    PasswordAuthentication no" >> /etc/ssh/ssh_config',
-    returns => [0,1],  
+	path	=> 'usr/bin:/bin',
+	command => 'echo "	IdentityFile ~/.ssh/school\n	PasswordAuthentication no" >> /etc/ssh/ssh_config',
+	returns => [0,1],
 }
