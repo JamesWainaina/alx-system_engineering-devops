c7dc7e332dff17150224f3c5a4dd0c5a8ee90c45
JamesWainaina <jamesgatheru9905@gmail.com>
2024-02-14 15:04:00 +0300

    subreddit response

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,11 +6,12 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Retrieving the number of subscribers of a given subreddit"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-        }
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    if subreddit:
+        url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+        headers = {
+            "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+            }
+        response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
     results = response.json().get("data")
