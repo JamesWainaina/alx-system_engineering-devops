a76b551c4526820b0d6a268a87dee37507156580
JamesWainaina <jamesgatheru9905@gmail.com>
2024-02-14 15:47:36 +0300

    printing the titles of the first ten posts for a given subredit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import requests
+
+def top_ten(subreddit):
+    """printing the titles of the first 10 hot posts"""
+    if subreddit is None or not isinstance(subreddit, str):
+        print("None")
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    params = {'limit':10}
+
+    response= requests.get(
+        url, headers=headers, params=params, allow_redirects=False)
+    data = response.json()
+    
+    try:
+        for post in data['data']['children']:
+            print(post['data']['title'])
+    except Exception:
+        print("None")
