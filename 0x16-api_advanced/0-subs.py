b57d4f789c4d0ea3c930a275eddfafb4db3f12b4
JamesWainaina <jamesgatheru9905@gmail.com>
2024-02-15 11:06:55 +0300

    return numer of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,10 +6,12 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Retrieving the number of subscribers of a given subreddit"""
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
                 "User-Agent": "MyCustomUserAgent/1.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    response = requests.get(url, headers=headers)
 
     data = response.json()
 
