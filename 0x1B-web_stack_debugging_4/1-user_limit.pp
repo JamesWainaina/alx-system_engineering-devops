2913033bf32b16320af1a365b1fdfd93b13d1070
JamesWainaina <jamesgatheru9905@gmail.com>
2024-02-23 12:00:29 +0000

    task 1

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,9 +1,12 @@
 #increasing the hard ans soft file config file for holberto user.
-exec { 'increase-hard-file-limit-for-holberton-user.
+
+exec { 'increase-hard-file-limit-for-holberton-user':
   command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
-  path    => 'usr/local/bin/:bin/'
+  path    => '/usr/local/bin/:/bin/',
 }
-exec { 'increase-soft-file-limit-for-holberton-user.
-  command => 'sed -i "/hoberton soft/s/4/50000/" /etc/security/limits.conf',
-  path    => 'usr/local/bin/:/bin/'
+
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/',
 }
+
